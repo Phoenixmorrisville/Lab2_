26c6d3ba84ef88c5559ea4df4e61a0c0195e6c0c
Phoenixmorrisville <mamrak387@morrisville.edu>
2026-09-18 03:59:25 -0400

    lab1

---
+++ Game01_Obstacle/Assets/Scripts/Dropper.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    [SerializeField] float timeToWait = 3f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > timeToWait)
+        {
+            Debug.Log("Lookout Below");
+        }
+        
+    }
+}
