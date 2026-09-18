763481031f0a7058ea8ae24863b5afdc67afb367
Phoenixmorrisville <mamrak387@morrisville.edu>
2026-09-18 07:14:00 -0400

    Added prefabs and reviewed execution order

--- Game01_Obstacle/Assets/Scripts/FlyAtPlayer.cs
+++ Game01_Obstacle/Assets/Scripts/FlyAtPlayer.cs
@@ -6,8 +6,13 @@ public class FlyAtPlayer : MonoBehaviour
     [SerializeField] Transform player;
     Vector3 playerPosition;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        gameObject.SetActive(false);
+    }
     void Start()
     {
+        
         playerPosition = player.transform.position;
     }
 
